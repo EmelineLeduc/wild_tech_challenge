173b0ca492cc464495f74fd042806d903662e62d
Emeline <emeline@vivlab.com>
2022-01-13 19:03:35 +0100

    Initialize Vue project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-light": "#f4f4f4",
+      },
+    },
+  },
+  plugins: [],
+};
