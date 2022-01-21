5cc428355b41744e8b58e11a4a79ffa80b865f7f
Emeline <leduc.emeline44@gmail.com>
2022-01-21 12:00:46 +0100

    index : create get and post routes

--- src/index.js
+++ src/index.js
@@ -17,6 +17,34 @@ connection.connect((err) => {
 app.use(cors("http://localhost:4000"));
 app.use(express.json());
 
+app.post("/api/argonaut", (req, res) => {
+  const { name } = req.body;
+  connection.query(
+    "INSERT INTO argonaut(name) VALUES (?)",
+    [name],
+    (err, result) => {
+      if (err) {
+        res.status(500).send("Error saving the argonaut");
+      } else {
+        res.status(200).send("Argonaut successfully saved");
+      }
+    }
+  );
+});
+
+app.get("/api/argonaut", (req, res) => {
+  connection
+    .promise()
+    .query("SELECT * FROM argonaut")
+    .then(([results]) => {
+      res.status(200).json(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving argonaut from database.");
+    });
+});
+
 app.listen(serverPort, (err) => {
   if (err) {
     console.error("Something bad happened");
